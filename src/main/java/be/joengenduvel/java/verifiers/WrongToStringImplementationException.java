e81edf0b38fc5c1cf397e2ef4e1a3e8f86633549
joengenduvel <robby.decosemaeker@telenet.be>
2015-08-24 07:27:17 +0200

    postfixed exception class with Exception

--- src/main/java/be/joengenduvel/java/verifiers/WrongToStringImplementationException.java
+++ src/main/java/be/joengenduvel/java/verifiers/WrongToStringImplementationException.java
@@ -2,14 +2,14 @@ package be.joengenduvel.java.verifiers;
 
 import java.lang.reflect.Field;
 
-public class WrongToStringImplementation extends RuntimeException {
+public class WrongToStringImplementationException extends RuntimeException {
 
 
-    public WrongToStringImplementation(String toString, Field privateDeclaredField, String fieldValue, Throwable throwable) {
+    public WrongToStringImplementationException(String toString, Field privateDeclaredField, String fieldValue, Throwable throwable) {
         super(String.format("Failed to find field '%s' with value '%s' in '%s because %s", privateDeclaredField.getName(), fieldValue, toString, throwable.getMessage()), throwable);
     }
 
-    public WrongToStringImplementation(Class<?> classToVerify, Throwable throwable) {
+    public WrongToStringImplementationException(Class<?> classToVerify, Throwable throwable) {
         super(String.format("Something went wrong during the validation of %s", classToVerify.getName()), throwable);
     }
 }
