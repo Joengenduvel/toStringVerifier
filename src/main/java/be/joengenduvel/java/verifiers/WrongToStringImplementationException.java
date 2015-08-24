8e8a9e568c36a55dd778e808669afeee21dde29a
joengenduvel <robby.decosemaeker@telenet.be>
2015-08-24 19:10:41 +0200

    improved error handling and messages

--- src/main/java/be/joengenduvel/java/verifiers/WrongToStringImplementationException.java
+++ src/main/java/be/joengenduvel/java/verifiers/WrongToStringImplementationException.java
@@ -5,11 +5,32 @@ import java.lang.reflect.Field;
 public class WrongToStringImplementationException extends RuntimeException {
 
 
-    public WrongToStringImplementationException(String toString, Field privateDeclaredField, String fieldValue, Throwable throwable) {
-        super(String.format("Failed to find field '%s' with value '%s' in '%s because %s", privateDeclaredField.getName(), fieldValue, toString, throwable.getMessage()), throwable);
+    private static final String OBJECT_NULL_MESSAGE = "Cannot verify '%s' because the object provided was null.";
+    private static final String FIELD_NOT_ACCESSABLE = "Could not verify field '%s' in class '%s' because %s";
+    private static final String VALUE_NOT_FOUND = "Could not find the value '%s' of '%s' in '%s' for class '%s'";
+    private static final String FIELD_NOT_FOUND = "Could not find field '%s' in '%s' for class '%s'";
+
+    private WrongToStringImplementationException(String message, Throwable cause) {
+        super(message, cause);
     }
 
-    public WrongToStringImplementationException(Class<?> classToVerify, Throwable throwable) {
-        super(String.format("Something went wrong during the validation of %s", classToVerify.getName()), throwable);
+    public WrongToStringImplementationException(String message) {
+        this(message, null);
+    }
+
+    public static WrongToStringImplementationException forObjectNull(Class<?> classToVerify) {
+        return new WrongToStringImplementationException(String.format(OBJECT_NULL_MESSAGE, classToVerify));
+    }
+
+    public static WrongToStringImplementationException forIllegalAccess(Field field, Class<?> classToVerify, IllegalAccessException cause) {
+        return new WrongToStringImplementationException(String.format(FIELD_NOT_ACCESSABLE, field.getName(), classToVerify, cause.getMessage()), cause);
+    }
+
+    public static WrongToStringImplementationException forValueNotFound(Field field, String fieldValue, Class<?> classToVerify, String toString) {
+        return new WrongToStringImplementationException(String.format(VALUE_NOT_FOUND, fieldValue, field.getName(), toString, classToVerify));
+    }
+
+    public static WrongToStringImplementationException forFieldNameNotFound(Field field, Class<?> classToVerify, String toString) {
+        return new WrongToStringImplementationException(String.format(FIELD_NOT_FOUND, field.getName(), toString, classToVerify));
     }
 }
